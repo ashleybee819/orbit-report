a3459f5a927c7ed66e01f687ce62e575a8bcf0b7
Ashley Brown <1thing.ashleyb@gmail.com>
2021-01-06 14:27:12 -0600

    commit after Bonus A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
@@ -25,8 +26,23 @@ export class AppComponent {
               fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
               this.sourceList.push(newSatellite);
           }
- 
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
+    
  }
+
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 }
